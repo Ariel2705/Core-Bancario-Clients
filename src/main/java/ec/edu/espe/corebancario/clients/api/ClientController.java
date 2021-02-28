6e9c2e0eeaf828cbb3574d9b6d7d4b4e18c22961
ariel2705 <haaltamirano1@espe.edu.ec>
2021-02-28 17:19:09 -0500

    Added Cross in classes of api package

--- src/main/java/ec/edu/espe/corebancario/clients/api/ClientController.java
+++ src/main/java/ec/edu/espe/corebancario/clients/api/ClientController.java
@@ -21,6 +21,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.repository.query.Param;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,6 +30,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(maxAge=3600)
 @RestController
 
 @RequestMapping("/api/corebancario/client")
